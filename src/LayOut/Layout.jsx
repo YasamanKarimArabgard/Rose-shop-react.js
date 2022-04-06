ebb4b6fd2b94ded59e70f748579f53b1ceb44e66
jasminearabgard <yasaman.arabgard@outlook.com>
2022-04-06 22:09:08 +0430

    create a base layout

--- src/LayOut/Layout.jsx
+++ src/LayOut/Layout.jsx
@@ -1,10 +1,13 @@
 import Navigation from "../components/Navigation";
+import './Layout.css'
 
 const Layout = ({ children }) => {
     return (
-        <div className="col-12">
+        <div className="layout col-12 bg-light">
             <Navigation />
-            {children}
+              <main className="col-12 d-flex flex-column align-items-center my-2">
+                  {children}
+              </main>
         </div>
     );
 };
