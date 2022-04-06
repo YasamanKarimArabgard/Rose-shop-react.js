6d4c55c39dfcc04d88c2acbcef4f3497ff5aca52
jasminearabgard <yasaman.arabgard@outlook.com>
2022-04-06 22:08:35 +0430

    add navigation

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,36 @@
 import Layout from "../LayOut/Layout";
+import * as data from '../data'
+import { useCartActions } from "../context/CartProvider";
 
 const HomePage = () => {
+
+    const dispatch = useCartActions();
+
+    const addToCartHandler = (product) => {
+        // console.log(product);
+        dispatch({type : 'Add_To_Cart', payload : product})
+    }
+
     return (
-          <Layout>
-              <p>this is home</p>
-          </Layout>
+        <Layout>
+            <main className="col-11">
+                <section className="product_list d-flex flex-wrap">
+                    {data.products.map(product => (
+                        <section className="product col-sm-2 bg-white m-1 p-1 rounded" key={product.id}>
+                            <div>
+                                <img src={product.image} className="w-100 h-auto"></img>
+                            </div>
+                            <div className="product-information col-12 d-flex justify-content-between align-items-center">
+                                <h6>{product.name}</h6>
+                                <p>${product.price}</p>
+                            </div>
+                            <button className="col-12 btn btn-sm btn-primary"
+                                onClick={() => addToCartHandler(product)}>add to cart</button>
+                        </section>
+                    ))}
+                </section>
+            </main>
+        </Layout>
     );
 };
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,19 @@
 import Navbar from 'react-bootstrap/Navbar';
-import { Container, Nav } from 'react-bootstrap';
+import { Container, Nav} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const items = [
-    { name: 'cart', to: '/cart' },
-    { name: 'home', to: '/' }
+    { name: 'home', to: '/' },
+    { name: 'cart', to: '/cart' }
 ]
 const Navigation = () => {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand href="#home">shop</Navbar.Brand>
                 <Nav className="me-auto">
                     {items.map(item => (
-                        <Nav.Link href={item.to}>{item.name}</Nav.Link>
+                        <Nav.Link to={item.to} as={NavLink}>{item.name}</Nav.Link>
                     ))}
                 </Nav>
             </Container>
@@ -20,4 +21,4 @@ const Navigation = () => {
     );
 };
 
-export default Navigation;;
+export default Navigation;
