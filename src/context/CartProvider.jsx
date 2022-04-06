ffab2b37f499b8610b3bce2d6097dc29ed2acca8
jasminearabgard <yasaman.arabgard@outlook.com>
2022-04-06 22:08:16 +0430

    create provider and reducer

---
+++ src/context/CartProvider.jsx
@@ -0,0 +1,28 @@
+import React, { useContext, createContext, useReducer } from 'react';
+import CartReducer from './CartReducer';
+
+const initialState = {
+    cart: [],
+    total: 0
+}
+
+const CartContext = createContext();
+const CartContextDispather = createContext();
+
+const CartProvider = ({ children }) => {
+
+    const [cart, dispatch] = useReducer(CartReducer, initialState);
+
+    return (
+        <CartContext.Provider value={cart}>
+            <CartContextDispather.Provider value={dispatch}>
+                {children}
+            </CartContextDispather.Provider>
+        </CartContext.Provider>
+    );
+};
+
+export default CartProvider;
+
+export const useCart = () => useContext(CartContext);
+export const useCartActions = () => useContext(CartContextDispather);
