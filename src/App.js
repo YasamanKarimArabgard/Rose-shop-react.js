c3c8082ae161b15038ae79a336a20fbb6c77557d
jasminearabgard <yasaman.arabgard@outlook.com>
2022-04-20 22:03:52 +0430

    import some files

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import {
 import HomePage from './Pages/HomePage';
 import Cart from './Pages/Cart/Cart';
 import CartProvider from "./context/CartProvider";
+import CheckoutPage from "./Pages/CheckoutPage";
+import SignupPage from "./Pages/SignupPage";
+import LoginPage from './Pages/LoginPage.jsx'
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<CheckoutPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/login' element={<LoginPage />} />
         </Routes>
       </CartProvider>
     </Router>
