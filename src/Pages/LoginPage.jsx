d42f90859cc909107fccfdd6a352705c9203f602
jasminearabgard <yasaman.arabgard@outlook.com>
2022-04-20 22:05:03 +0430

    add login page

---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Layout from "../LayOut/Layout";
+import Login from "../components/Login";
+
+const LoginPage = () => {
+    return (
+        <Layout>
+            <Login />
+        </Layout>
+    );
+};
+
+export default LoginPage;
