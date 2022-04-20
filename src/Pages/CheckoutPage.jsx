25da217bae4d877f8f53603b6e4c72eb46e55b14
jasminearabgard <yasaman.arabgard@outlook.com>
2022-04-20 22:15:03 +0430

    add check out page

---
+++ src/Pages/CheckoutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../LayOut/Layout';
+
+const CheckoutPage = () => {
+    return (
+        <Layout>
+            <p> checkout page </p>
+        </Layout>
+    );
+};
+
+export default CheckoutPage;
