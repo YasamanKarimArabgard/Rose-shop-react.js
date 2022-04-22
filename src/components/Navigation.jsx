746ec377afa9e55573d15033431044499d2f5fcd
jasminearabgard <yasaman.arabgard@outlook.com>
2022-04-22 17:22:12 +0430

    add dropdown

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import Navbar from 'react-bootstrap/Navbar';
-import { Container, Nav } from 'react-bootstrap';
+import { Container, Nav, NavDropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useCart } from '../context/CartProvider';
 
@@ -11,13 +11,20 @@ const Navigation = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">shop</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link to='/' as={NavLink}>Home</Nav.Link>
-                    <Nav.Link to='/cart' as={NavLink} className='position-relative'>Cart
-                        {cart.length ? <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger mt-3 mx-1">{cart.length}</span> : null }
-                    </Nav.Link>
-                </Nav>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+                        <Nav.Link to="/cart" as={NavLink}>cart
+                            {cart.length ? <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger mt-3 mx-2">{cart.length}</span> : null}
+                        </Nav.Link>
+                        <NavDropdown title="Account" id="basic-nav-dropdown">
+                            <NavDropdown.Item to="/signup" as={NavLink}>Signup</NavDropdown.Item>
+                            <NavDropdown.Item to="/login" as={NavLink}>login</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
