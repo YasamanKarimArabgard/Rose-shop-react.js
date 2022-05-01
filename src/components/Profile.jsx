131f3c90b5b6b2be51dedeb9e3c1b69d767c3aa2
jasminearabgard <yasaman.arabgard@outlook.com>
2022-05-02 00:10:06 +0430

    create profile page in basic

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useAuth, useAuth } from '../context/AuthProvider';
+
+const Profile = () => {
+
+    const useAuth = useAuth();
+    return (
+        <div>
+            hi
+        </div>
+    );
+};
+
+export default Profile;
