cb6b664296d6e6ec2e699c40423a42c670394c2a
jasminearabgard <yasaman.arabgard@outlook.com>
2022-05-02 00:08:31 +0430

    install some package

--- src/App.js
+++ src/App.js
@@ -3,30 +3,44 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-// import Layout from './LayOut/Layout';
 import HomePage from './Pages/HomePage';
 import Cart from './Pages/Cart/Cart';
 import CartProvider from "./context/CartProvider";
 import CheckoutPage from "./Pages/CheckoutPage";
 import SignupPage from "./Pages/SignupPage";
 import LoginPage from './Pages/LoginPage.jsx'
-import AuthProvider from "./context/AuthProvider";
+import AuthProvider, { useAuth } from "./context/AuthProvider";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
+
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#84a59d'
+      },
+    },
+  });
+
+
+  const auth = useAuth();
   return (
-    <Router>
-      <AuthProvider>
-        <CartProvider>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/checkout' element={<CheckoutPage />} />
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/login' element={<LoginPage />} />
-          </Routes>
-        </CartProvider>
-      </AuthProvider>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AuthProvider>
+          <CartProvider>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/checkout' element={<CheckoutPage />} />
+              <Route path='/signup' element={<SignupPage />} />
+              <Route path='/login' element={<LoginPage />} />
+            </Routes>
+          </CartProvider>
+        </AuthProvider>
+      </Router>
+    </ThemeProvider>
   );
 }
 
