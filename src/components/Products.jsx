16e17e31e54b562afa1609dd07338e335751647a
jasminearabgard <yasaman.arabgard@outlook.com>
2022-05-17 17:12:42 +0430

    add search box

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
-import * as data from '../data';
-import { Button, IconButton } from "@mui/material";
-import { useCartActions, useCart } from "../context/CartProvider";
+// import * as data from '../data';
+import { IconButton } from "@mui/material";
+import { useCartActions } from "../context/CartProvider";
 import { checkInCart } from "../utils/CheckInCart";
 import { ToastAlert } from "./ToastAlert";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Searchbar from './Searchbar';
+import { useProducts } from '../context/ProductsProvider';
 
 const Products = () => {
 
-    const [open, setOpen] = useState(false);
+    const products = useProducts();
 
+    const [open, setOpen] = useState(false);
+    const [filteredItems, setFilteredItems] = useState(products)
+
+    // console.log(products);
     const dispatch = useCartActions();
 
     const addToCartHandler = (product) => {
@@ -17,19 +23,21 @@ const Products = () => {
         dispatch({ type: 'Add_To_Cart', payload: product })
     }
 
-    const handleClose = (event, reason) => {
+    const handleClose = (reason) => {
         if (reason === 'clickaway') {
             return;
         }
-
         setOpen(false);
     };
 
     return (
         <>
-            <main className="products_container col-12">
+            <main className="products_container col-12 d-flex flex-column align-items-center">
+                <section className='searchbar_container col-9 d-flex justify-content-center'>
+                    <Searchbar products={products} filteredItems={filteredItems} setFilteredItems={setFilteredItems} />
+                </section>
                 <section className="product_list col-12 d-flex flex-wrap justify-content-center mb-2">
-                    {data.products.map(product => (
+                    {filteredItems.map(product => (
                         <section className="product col-5 col-md-3 bg-white rounded m-1 p-2" key={product.id}>
                             <div>
                                 <img src={product.image} className="w-100 h-auto"></img>
@@ -39,7 +47,7 @@ const Products = () => {
                                     <h6 className='col-12'>{product.name}</h6>
                                 </div>
                                 <div className='col-12 d-flex align-items-end justify-content-between'>
-                                    <h6 className='col-9'>{product.price} $</h6>
+                                    <h6 className='col-9'>{product.price}$</h6>
                                     <div className='col-3 d-flex justify-content-end'>
                                         <IconButton
                                             sx={{ bgcolor: 'lightPink.main' }}
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+const Searchbar = ({ products, filteredItems, setFilteredItems }) => {
+
+    const [searchItems, setSearchItems] = useState('');
+
+    const filteredProducts = (search) => {
+        if (!search || search === '') {
+            setFilteredItems(products)
+            return;
+        }
+        const filtered = products.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+        setFilteredItems(filtered)
+    }
+
+    const onSearchHandler = (e) => {
+        setSearchItems(e.target.value)
+        filteredProducts(e.target.value)
+    }
+
+    useEffect(() => {
+        filteredProducts(searchItems)
+    }, [products])
+
+
+    return (
+        <>
+            <input
+                className='form-control shadow-none'
+                type='text'
+                placeholder='Search here...'
+                onChange={onSearchHandler}
+            />
+        </>
+    );
+};
+
+export default Searchbar;
