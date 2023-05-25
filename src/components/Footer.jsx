ff9d2132503ecc7d81e9199a53b2501af13b7667
jasminearabgard <yasaman.arabgard@outlook.com>
2023-05-25 19:27:12 +0330

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className='col-12'>
             <Box bgcolor='primary.main' sx={{ height: '2rem' }} >
-                <Typography color='#cbcbcb' align="center" py={1} sx={{ fontSize: '0.6em' }}>Created by @Yasaman_Arabgard since 2022</Typography>
+                <Typography color='#cbcbcb' align="center" py={1} sx={{ fontSize: '0.6em' }}>Created by @Yasaman_Arabgard</Typography>
             </Box>
         </footer>
     );
