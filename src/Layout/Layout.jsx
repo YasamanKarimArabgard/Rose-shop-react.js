f12e909694b304420ce9aefbf5a9c4d6cbe30b60
jasminearabgard <yasaman.arabgard@outlook.com>
2023-06-16 18:59:51 +0330

    layout by tailwind

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import Footer from "../components/Footer";
-import Navigation from "../components/Navigation/Navigation"
-import './Layout.css'
-import { Box } from "@mui/material";
+import Naviagation from "../components/Navigation/Naviagation";
 
 const Layout = ({ children }) => {
     return (
-        <div className="layout_container col-12 bg-light d-flex flex-column">
-            <Navigation />
-            <main className="main-layout col-12 d-flex flex-column align-items-center">
+        <div className="layout_container col-span-12 bg-purple-50 max-h-screen h-auto">
+            <Naviagation/>
+            <main className="main-layout col-span-12 min-h-screen h-auto">
                     {children}
             </main>
             <Footer />
