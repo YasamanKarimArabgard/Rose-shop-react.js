0f755892b75b3fd50a44e93b2bceb6c63bfd2b5b
jasminearabgard <yasaman.arabgard@outlook.com>
2023-06-30 20:34:02 +0330

    add width 100% into footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import { Box, Typography } from "@mui/material";
 
 const Footer = () => {
     return (
-        <footer className='col-12'>
+        <footer className="w-screen">
             <Box bgcolor='primary.main' sx={{ height: '2rem' }} >
-                <Typography color='#cbcbcb' align="center" py={1} sx={{ fontSize: '0.6em' }}>Created by @Yasaman_Arabgard</Typography>
+                <Typography color='#cbcbcb' align="center" py={1} sx={{ fontSize: '0.6em' }}>Developed by @Yasaman_Arabgard</Typography>
             </Box>
         </footer>
     );
