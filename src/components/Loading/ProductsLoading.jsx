9917f9d528a635956b5390b036fbc625fce460d4
jasminearabgard <yasaman.arabgard@outlook.com>
2023-07-01 01:25:15 +0330

    add loading spinner

--- src/components/Loading/ProductsLoading.jsx
+++ src/components/Loading/ProductsLoading.jsx
@@ -1,22 +1,12 @@
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const ProductsLoading = () => {
     return (
-        <SkeletonTheme baseColor='#ebebeb' height={300}>
-            {
-                Array(6).fill().map((p, index) => {
-                   return <section className='col-span-10'>
-                        <p>
-                            <Skeleton count={1} height={150} />
-                        </p>
-                        <p>
-                            <Skeleton count={5} height={10} />
-                        </p>
-                    </section>
-                })
-            }
-        </SkeletonTheme>
-    );
+        <div className='col-span-10 row-start-7 flex justify-center items-center'>
+            <CircularProgress color='primary' size='3rem' />
+            <span className='ml-2 text-2xl font-bold text-purple-800'>Loading...</span>
+        </div>
+    )
 }
 
 export default ProductsLoading;
