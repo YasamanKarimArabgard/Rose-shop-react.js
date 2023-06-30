2427df96a68c672be751c6000abd38656ec292d6
jasminearabgard <yasaman.arabgard@outlook.com>
2023-06-30 21:05:06 +0330

    add error img and refresh button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -8,6 +8,7 @@ import SelectPrice from './SelectPrice';
 import Product from './Product';
 import ProductsLoading from '../components/Loading/ProductsLoading';
 import HomeBanner from '../components/Home/HomeBanner';
+import emptyCartImg from '../assets/images/emptycart (2).png'
 
 const Products = () => {
 
@@ -66,13 +67,13 @@ const Products = () => {
                         setFilteredItems={setFilteredItems}
                     />
                 </section>
-                <section id='products' className="product_list col-start-2 flex">
+                <section id='products' className="product_list col-start-2 flex justify-center">
                     <section className='grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 md:p-0 sm:gap-x-4 md:gap-x-6 xl:gap-x-8 md:gap-y-3 mb-2 my-3'>
 
                         {/* first check error then check categories */}
                         {
                             error ?
-                                <h3 className='text-danger mt-5'>The Server is not responding!</h3> :
+                                <ServerError /> :
                                 filteredItems.length === 0 ?
                                     <ProductsLoading /> :
                                     filteredItems.map(product => (
@@ -92,3 +93,26 @@ const Products = () => {
 };
 
 export default Products;
+
+const ServerError = () => {
+
+    function refreshPage() {
+        window.location.reload(false);
+    }
+
+    return (
+        <section className='col-span-12 col-start-2 row-start-4 flex justify-center min-h-screen'>
+            <div className='flex justify-center items-start'>
+                <div className='flex flex-col-reverse items-center md:flex-row md:justify-center'>
+                    <div className='flex flex-col justify-center items-center xl:w-2/5'>
+                        <h5 className='mt-5 mb-4 text-red-500 text-md xl:text-lg font-bold'>Server isn't respondeing!</h5>
+                        <button className='btn w-2/3 p-1 md:w-32 text-sm md:py-2 border border-purple-500 text-purple-500 hover:bg-purple-100 rounded-xl' onClick={()=>refreshPage()}>Try again!</button>
+                    </div>
+                    <div className='w-2/3 md:w-1/2 xl:w-3/5 ml-3 flex justify-center'>
+                        <img className='w-full md:w-3/4 h-auto' src={emptyCartImg} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
