193a67e8744b21b1a0d4b007dbc97763877910c3
jasminearabgard <yasaman.arabgard@outlook.com>
2023-07-01 17:27:56 +0330

    remove link

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "react-loading-skeleton/dist/skeleton.css";
 
 ReactDOM.render(
   <React.StrictMode>
